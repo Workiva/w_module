b6ec49f02dab020878b4e8423caaecb7eddcb2e6
Todd Beckman <todd.beckman@workiva.com>
2018-09-07 12:32:09 -0600

    setup comparing files

--- test/bin/analyzer_tools_test.dart
+++ test/bin/analyzer_tools_test.dart
@@ -1,74 +1,114 @@
+import 'dart:io';
+
 import 'package:analyzer/dart/element/element.dart';
 import 'package:test/test.dart';
 
 import 'fixtures.dart';
 
 void main() {
-  group('AnalyzerTools', () {
-    const String accessorsName = 'accessors.dart';
-    group(accessorsName, () {
-      List<ClassElement> classes;
+  group('name_upgrader', () {
+    setUpAll(() {
+      createTempDir();
+    });
 
-      setUp(() {
-        classes = modulesForSourceName(accessorsName);
-      });
+    tearDownAll(() {
+      clearTempDir();
+    });
 
-      [
-        'ModuleWithNameSetter',
-      ].forEach((className) {
-        test('catches $className', () {
-          expect(findClassByName(classes, className), isNotNull);
+    group('AnalyzerTools', () {
+      const String accessorsName = 'accessors.dart';
+      group(accessorsName, () {
+        List<ClassElement> classes;
+
+        setUp(() {
+          classes = modulesForSourceName(accessorsName);
+        });
+
+        [
+          'ModuleWithNameSetter',
+        ].forEach((className) {
+          test('catches $className', () {
+            expect(findClassByName(classes, className), isNotNull);
+          });
+        });
+
+        [
+          'ModuleWithNameGetter',
+          'ModuleWithNameGetterSetter',
+          'ModuleWithNameField',
+          'ModuleWithFinalNameField',
+        ].forEach((className) {
+          test('does not catch $className', () {
+            expect(() => findClassByName(classes, className),
+                throwsA(isException));
+          });
         });
       });
 
-      [
-        'ModuleWithNameGetter',
-        'ModuleWithNameGetterSetter',
-        'ModuleWithNameField',
-        'ModuleWithFinalNameField',
-      ].forEach((className) {
-        test('does not catch $className', () {
-          expect(
-              () => findClassByName(classes, className), throwsA(isException));
+      const String inheritanceName = 'inheritance.dart';
+      group(inheritanceName, () {
+        List<ClassElement> classes;
+
+        setUp(() {
+          classes = modulesForSourceName(inheritanceName);
+        });
+
+        [
+          'UnnamedModule',
+          'UnnamedModuleExtendsFromNamedModule',
+          'UnnamedModuleExtendsFromUnnamedModule',
+          'UnnamedUnnamedUnnamed',
+          'UnnamedUnnamedNamed',
+          'UnnamedNamedUnnamed',
+          'UnnamedNamedNamed',
+        ].forEach((className) {
+          test('catches $className', () {
+            expect(findClassByName(classes, className), isNotNull);
+          });
+        });
+
+        [
+          'NamedModule',
+          'NamedModuleExtendsFromNamedModule',
+          'NamedModuleExtendsFromUnnamedModule',
+          'NamedUnnamedUnnamed',
+          'NamedUnnamedNamed',
+          'NamedNamedUnnamed',
+          'NamedNamedNamed',
+        ].forEach((className) {
+          test('does not catch $className', () {
+            expect(() => findClassByName(classes, className),
+                throwsA(isException));
+          });
         });
       });
     });
 
-    const String inheritanceName = 'inheritance.dart';
-    group(inheritanceName, () {
-      List<ClassElement> classes;
+    test('files are updated to match expected test_output', () {
+      final Directory temp = new Directory('$tempDirPath/lib');
+      expect(temp.existsSync(), isTrue);
+      final List<FileSystemEntity> tempFiles =
+          temp.listSync(recursive: true, followLinks: false);
 
-      setUp(() {
-        classes = modulesForSourceName(inheritanceName);
-      });
+      final Directory expected = new Directory('$expectedOutputDirPath/lib');
+      expect(expected.existsSync(), isTrue);
 
-      [
-        'UnnamedModule',
-        'UnnamedModuleExtendsFromNamedModule',
-        'UnnamedModuleExtendsFromUnnamedModule',
-        'UnnamedUnnamedUnnamed',
-        'UnnamedUnnamedNamed',
-        'UnnamedNamedUnnamed',
-        'UnnamedNamedNamed',
-      ].forEach((className) {
-        test('catches $className', () {
-          expect(findClassByName(classes, className), isNotNull);
-        });
-      });
+      final List<FileSystemEntity> expectedFiles =
+          expected.listSync(recursive: true, followLinks: false);
 
-      [
-        'NamedModule',
-        'NamedModuleExtendsFromNamedModule',
-        'NamedModuleExtendsFromUnnamedModule',
-        'NamedUnnamedUnnamed',
-        'NamedUnnamedNamed',
-        'NamedNamedUnnamed',
-        'NamedNamedNamed',
-      ].forEach((className) {
-        test('does not catch $className', () {
-          expect(
-              () => findClassByName(classes, className), throwsA(isException));
-        });
+      expect(tempFiles.length, expectedFiles.length);
+
+      File tempFile, expectedFile;
+      String tempContents, expectedContents;
+      tempFiles.where((entity) => entity is File).forEach((entity) {
+        tempFile = entity;
+        expectedFile = expectedFiles.firstWhere((entity) =>
+            fileNameFromPath(entity.path) == fileNameFromPath(tempFile.path));
+
+        tempContents = tempFile.readAsStringSync();
+        expectedContents = expectedFile.readAsStringSync();
+
+        expect(tempContents, expectedContents);
       });
     });
   });
--- test/bin/fixtures.dart
+++ test/bin/fixtures.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:test/test.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/src/generated/source.dart'; // ignore: implementation_imports
 import 'package:w_module/src/bin/analyzer_tools.dart';
 
-const String everythingPackageDir = 'test_fixtures/everything';
+const String tempDirPath = 'test_fixtures/test_temp';
+const String inputDirPath = 'test_fixtures/test_input';
+const String expectedOutputDirPath = 'test_fixtures/test_expected';
 
 Map<Source, List<ClassElement>> _modulesWithoutNames;
 
@@ -13,7 +16,7 @@ Map<Source, List<ClassElement>> get modulesWithoutNames {
     return _modulesWithoutNames;
   }
 
-  Directory packageDir = new Directory(everythingPackageDir);
+  Directory packageDir = new Directory(tempDirPath);
 
   _modulesWithoutNames = getModulesWithoutNamesBySource(packageDir: packageDir);
   return _modulesWithoutNames;
@@ -37,3 +40,21 @@ ClassElement findClassByName(List<ClassElement> classes, String name) {
 
   throw new Exception('Class not found: $name');
 }
+
+void createTempDir() {
+  copyDirCommand(inputDirPath, tempDirPath);
+}
+
+void clearTempDir() {
+  final directory = new Directory(tempDirPath);
+  if (directory.existsSync()) {
+    directory.deleteSync(recursive: true);
+  }
+}
+
+void copyDirCommand(String from, String to) {
+  // because doing this in dart is ~30 lines
+  Process.runSync('cp', ['-r', from, to]);
+}
+
+String fileNameFromPath(String path) => path.split('/').last;
